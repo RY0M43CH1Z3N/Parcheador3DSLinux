19b6f32150bde647cea3e634a5567533214c8ef6
Darkmet <iinfernape1998@outlook.es>
2018-04-09 09:46:29 +0200

    Removed shc64

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#Instalando los paquetes necesarios
-#sudo apt install cmake libiconv-hook-dev libiconv-hook-dev openssl libssl-dev libcurl4-openssl-dev
 
 #Creando carpetas necesarias
 mkdir build
@@ -11,12 +9,10 @@ cd "sources"
 git clone --recursive https://github.com/megastep/makeself.git
 
 #Compilando script principal
+mkdir -p "../build/out"
 cd "script"
-cat main.sh cia.sh 3ds.sh luma.sh ntr.sh check.sh > "../../build/script.sh"
+cat main.sh cia.sh 3ds.sh luma.sh ntr.sh check.sh > "../../build/out/script.sh"
 cd ../..
-#Cifrando script principal
-mkdir "build/out"
-"sources/tools/shc64" -o "build/out/parcheador.run" -f "build/script.sh"
 
 #Creando carpetas necesarias
 mkdir -p "build/out/temp/extraido"
@@ -28,4 +24,4 @@ cp -r "sources/patch" "build/out/parches"
 cp -r "sources/prebuild" "build/out/binarios"
 
 #Comprimiendo todo
-"sources/makeself/makeself.sh" "build/out" "out/ParcheadorLOM.run" "Parcheador Lord Of Magna - Maiden Heaven español" ./parcheador.run
+"sources/makeself/makeself.sh" "build/out" "out/Parcheador.run" "Parcheador español" ./script.sh
